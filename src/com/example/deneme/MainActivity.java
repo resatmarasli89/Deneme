129c4c1a5a65320e85579072882df54e09cd2405
resatmarasli89 <resatmarasli@gmail.com>
2014-02-21 16:11:14 +0200

    toast eklendi.

--- src/com/example/deneme/MainActivity.java
+++ src/com/example/deneme/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.deneme;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +11,9 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Toast.makeText(MainActivity.this, "Hello World", Toast.LENGTH_SHORT).show();
+		
 	}
 
 }
