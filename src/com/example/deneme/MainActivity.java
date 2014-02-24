e1ca5257c4c5280ff759190e9a8c939612a37aed
Erdi Köse <erdikose8@hotmail.com>
2014-02-24 11:21:44 +0200

    countly

--- src/com/example/deneme/MainActivity.java
+++ src/com/example/deneme/MainActivity.java
@@ -11,14 +11,28 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		Toast.makeText(MainActivity.this, " !!Hello Mars 11Bir1111!!!!", Toast.LENGTH_SHORT)
-				.show();
+		Toast.makeText(MainActivity.this, " !!Hello Mars 11Bir1111!!!!",
+				Toast.LENGTH_SHORT).show();
 
 		Toast.makeText(MainActivity.this, "Bir toast da benden",
 				Toast.LENGTH_SHORT).show();
-		
-		
+
+		Countly.sharedInstance().init(MainActivity.this,
+				"https://cloud.count.ly", "53033767172bb08c52038fb6");
 
 	}
 
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		Countly.sharedInstance().onStart();
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+		Countly.sharedInstance().onStop();
+	}
 }
