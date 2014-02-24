ff9ab4c50f186b67e04dcd5292de9d3acebdf83e
Erdi Köse <erdikose8@hotmail.com>
2014-02-24 10:35:43 +0200

    dsfs

---
+++ src/com/example/deneme/yeni/Yeni.java
@@ -0,0 +1,16 @@
+package com.example.deneme.yeni;
+
+public class Yeni {
+	String YeniStr;
+	int Yeniint;
+	boolean yenibool;
+	public Yeni(String yeniStr, int yeniint, boolean yenibool) {
+		super();
+		YeniStr = yeniStr;
+		Yeniint = yeniint;
+		this.yenibool = yenibool;
+	}
+	
+	
+
+}
