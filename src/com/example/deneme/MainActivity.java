aa5175f62246147b3521d90d7d02fb873e09eaf8
resatmarasli89 <resatmarasli@gmail.com>
2014-02-24 10:55:49 +0200

    bir!!!

--- src/com/example/deneme/MainActivity.java
+++ src/com/example/deneme/MainActivity.java
@@ -11,11 +11,13 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		Toast.makeText(MainActivity.this, "Hello World", Toast.LENGTH_SHORT)
+		Toast.makeText(MainActivity.this, " !!Hello Mars 11Bir1111!!!!", Toast.LENGTH_SHORT)
 				.show();
 
 		Toast.makeText(MainActivity.this, "Bir toast da benden",
 				Toast.LENGTH_SHORT).show();
+		
+		
 
 	}
 
