02a810e3037b4abe381b34055256dd92cad732cd
Erdi Köse <erdikose8@hotmail.com>
2014-02-24 10:07:14 +0200

    deneme şeettim

--- src/com/example/deneme/MainActivity.java
+++ src/com/example/deneme/MainActivity.java
@@ -11,9 +11,13 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		Toast.makeText(MainActivity.this, "Hello World", Toast.LENGTH_SHORT).show();
-		
+
+		Toast.makeText(MainActivity.this, "Hello World", Toast.LENGTH_SHORT)
+				.show();
+
+		Toast.makeText(MainActivity.this, "Bir toast da benden",
+				Toast.LENGTH_SHORT).show();
+
 	}
 
 }
